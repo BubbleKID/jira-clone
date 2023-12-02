5a6d043da36e20a077f7cb1b22ed96d9a9442721
Mark Chen <>
2023-12-02 12:23:16 +1100

    update ui

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,6 +9,7 @@ function Header() {
   return (
     <header className="w-full">
         <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl">
+            <div className="absolute left-0 top-0 w-full h-96 bg-gradient-to-br from-pink-400 to-[#0055d1] rounded-md filter blur-3xl opacity-50 -z-50"></div>
             <Image
                 src="/Trello_logo.svg.png"
                 alt="Trello Logo"
