ffec8b3150de35f2257f64b3aaa0ac2467049c42
Mark <fqchenxin@gmail.com>
2023-12-05 09:00:10 +1100

    update input

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,8 +4,14 @@ import React from 'react'
 import Image from 'next/image';
 import { MagnifyingGlassIcon, UserCircleIcon } from '@heroicons/react/20/solid';
 import Avatar from 'react-avatar';
+import { useBoardStore } from '@/store/BoardStore';
 
 function Header() {
+  const [searchString, setSearchString] = useBoardStore((state) => [
+      state.searchString,
+      state.setSearchString,
+  ]);
+
   return (
     <header className="w-full">
         <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl">
@@ -21,7 +27,12 @@ function Header() {
                 {/* Search Box */}
                 <form className="flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial">
                     <MagnifyingGlassIcon className="w-6 h-6 text-gray-400" />
-                    <input type="text" placeholder='Search' className="felx-1 outline-none p-2" />
+                    <input 
+                      type="text" 
+                      placeholder='Search' 
+                      className="felx-1 outline-none p-2" 
+                      onChange={e => setSearchString(e.target.value)}
+                    />
                     <button type="submit" hidden>
                         Search
                     </button>
