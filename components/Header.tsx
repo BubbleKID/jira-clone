dc8f54db6da62ab4e6ff120cc02109715dace08b
Mark <fqchenxin@gmail.com>
2023-12-07 00:44:23 +1100

    update ui

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,16 +14,16 @@ function Header() {
 
   return (
     <header className="w-full">
-        <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl">
+        <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10">
             <div className="absolute left-0 top-0 w-full h-96 bg-gradient-to-br from-pink-400 to-[#0055d1] rounded-md filter blur-3xl opacity-50 -z-50"></div>
             <Image
                 src="/Trello_logo.svg.png"
                 alt="Trello Logo"
                 width={300}
                 height={100}
-                className="w-44 md:w-56 pb-10 md:pb-0 object-contain"
+                className="w-44 md:w-56 pb-10 md:pb-0 object-contain mr-5"
             />
-            <div className="flex items-center space-x-5 flex-1"> 
+            <div className="flex items-center space-x-5 flex-1 w-full justify-end"> 
                 {/* Search Box */}
                 <form className="flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial">
                     <MagnifyingGlassIcon className="w-6 h-6 text-gray-400" />
